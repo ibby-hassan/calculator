03209a833e40003d71c7d23f0c12b13d91a7e408
Ibrahim <ibrahim.hassan2402@gmail.com>
2024-10-11 03:43:49 +0100

    Basic arithmetic operations now work and display. Could do with more testing.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Calculator
 A functional calculator made with HTML, CSS and JavaScript
+
+Backlog:
+* Decimal functionality
+* Parenthesis functionality
+* Enable functionality to allow result from an expression to be immediately operated on
+* Create an active display for actions in the array 
+* Convert the input array into a stack
+* Write tests
+
+
+Finished Features:
+* Create UI
+* Allow input to screen from number keys
+* Create expression construction with operation keys 
+
--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let displayScreen = document.querySelector('.calculator-screen');
 let inputString = '';
+let inputArray = [];
 
 //Tested, works. 
-let numberKeys = document.querySelectorAll('.number-key');
+let numberKeys = document.querySelectorAll('.number');
 numberKeys.forEach(key => {
     key.addEventListener('click', function() {
         inputString += key.value;
@@ -11,8 +12,43 @@ numberKeys.forEach(key => {
 });
 
 //Tested, works.
-let clearKey = document.querySelector('button[value="all-clear"]');
+let operatorKeys = document.querySelectorAll('.operator');
+operatorKeys.forEach(key => {
+    key.addEventListener('click', function() {
+        if (inputString == '') {
+            return;
+        }
+        inputArray.push(inputString);
+        inputArray.push(key.value);
+        inputString = '';
+        displayScreen.value = inputString;
+    });
+});
+
+//Tested, works.
+let allClearKey = document.querySelector('button[value="all-clear"]');
+allClearKey.addEventListener('click', function() {
+    inputString = '';
+    inputArray = [];
+    displayScreen.value = inputString;
+});
+
+//Tested, works.
+let clearKey = document.querySelector('button[value="clear"]');
 clearKey.addEventListener('click', function() {
     inputString = '';
     displayScreen.value = inputString;
+});
+
+let equalsKey = document.querySelector('.equal-sign');
+equalsKey.addEventListener('click', function() {
+    if (inputString == '') {
+        return;
+    }
+    inputArray.push(inputString);
+    let equation = inputArray.join('');
+    let result = eval(equation);
+    displayScreen.value = String(result);
+    inputArray = [];
+    inputString = '';
 });
